a5ab89f91426270e48ceac037466d7ac7f6af143
JeyTix <tixi4615@gmail.com>
2024-10-12 12:46:01 -0500

    Comenzando

---
+++ src/Menu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void menus(){
+        Scanner xd= new Scanner(System.in);
+        int opcion;
+
+        System.out.println("\t----- MENU -----");
+        System.out.println("1. Ingresar arreglo");
+        System.out.println("2. Ordenar arreglo");
+        System.out.println("3. Salir");
+        System.out.println("Ingrese una opcion: ");
+        opcion= xd.nextInt();
+
+        switch(opcion){
+            case 1:
+            System.out.println("\tIngresar Arreglo");
+            System.out.println("Ingrese el tamano: ");
+            int tamano= xd.nextInt();
+        
+            System.out.println("Ingrese ");
+        
+        
+        }
+        
+        
+    }
+}
